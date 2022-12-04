d35c990e9da06ffb66d7b556564a82557387a2a9
Yarytsky <48153056+Yarytsky@users.noreply.github.com>
2022-12-04 18:58:22 +0200

    Add files via upload

---
+++ hometask2.py
@@ -0,0 +1,26 @@
+from ECurve import ECurve, ECPoint
+# За потребності можна зробити запит на ввід з клавіатури, але це тест, щоб ви могли зразу запустити
+
+curve = ECurve(3, 4)
+print(curve.EquationToString())
+curve.PrintEquation()
+
+point1 = ECPoint(0, 2)
+point1.PrintECPoint()
+print(point1.ECPointToString(), "- точка що існує на еліптичній кривій" if point1.IsOnCurveCheck(curve) else "- точка яка не належить еліптичній кривій")
+
+point2 = ECPoint(-1, 0)
+point2.PrintECPoint()
+print(point2.ECPointToString(), "- точка що існує на еліптичній кривій" if point2.IsOnCurveCheck(curve) else "- точка яка не належить еліптичній кривій")
+
+point3 = curve.AddECPoints(point1, point2)
+point3.PrintECPoint()
+print(point3.ECPointToString(), "- точка що існує на еліптичній кривій" if point3.IsOnCurveCheck(curve) else "- точка яка не належить еліптичній кривій")
+
+point4 = point3.DoubleECPoint(curve)
+point4.PrintECPoint()
+print(point4.ECPointToString(), "- точка що існує на еліптичній кривій" if point4.IsOnCurveCheck(curve) else "- точка яка не належить еліптичній кривій")
+
+point5 = point3.ScalarMult(3)
+point5.PrintECPoint()
+print(point5.ECPointToString(), "- точка що існує на еліптичній кривій" if point5.IsOnCurveCheck(curve) else "- точка яка не належить еліптичній кривій")
